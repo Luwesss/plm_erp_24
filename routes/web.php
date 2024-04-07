63fb827befde85117198719b72864eb87cc77361
aleurry <aleurrycasas@gmail.com>
2024-04-07 17:59:57 +0800

    feat: done superadmin

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Livewire\Counter;
 use App\Livewire\Login;
 use App\Livewire\Saam;
+use App\Livewire\Superadmin;
 
 Route::get('/', function () {
     return view('welcome');
@@ -14,3 +15,5 @@ Route::get('/counter', Counter::class);
 Route::get('/login', Login::class);
 
 Route::get('/saam', Saam::class);
+
+Route::get('/sa', Superadmin::class);
