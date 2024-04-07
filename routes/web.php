7b79d119d99dbbc425395c55abc5f63d54aae3ae
aleurry <aleurrycasas@gmail.com>
2024-04-07 20:02:13 +0800

    feat: permissions

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Livewire\Counter;
 use App\Livewire\Login;
 use App\Livewire\Saam;
 use App\Livewire\Userslist;
+use App\Livewire\Permissions;
 
 Route::get('/', function () {
     return view('welcome');
@@ -17,3 +18,5 @@ Route::get('/login', Login::class);
 Route::get('/saam', Saam::class);
 
 Route::get('/ul', Userslist::class);
+
+Route::get('/p', Permissions::class);
