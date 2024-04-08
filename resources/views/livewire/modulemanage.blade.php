d558eb7fe8db73e833e5f9ee511bd89d2bf3164b
vincicoed <vinjove323@gmail.com>
2024-04-08 13:58:14 -0800

    style: Modulemanage

--- resources/views/livewire/modulemanage.blade.php
+++ resources/views/livewire/modulemanage.blade.php
@@ -1,7 +1,36 @@
-<div>
-    <div style="text-align: center; padding-top: 1rem;  font-size: 25px;">  <strong style="font-weight: bold;">Module Admin: Account Permission Management</strong>
+<div style="background-image: url('bg.png'); background-size: cover; height: 100vh; color: black;">
+
+    <div class="navbar bg-base-100 bg-white">
+        <div class="flex-1">
+          <div class="h-full w-20">
+            <img src="plm_logo.png" alt="PLM Logo" class="object-contain h-full w-full">
+          </div>
+        </div>
+        <div class="flex-none gap-2">
+          <div class="dropdown">
+            <button tabindex="0" class="btn btn-ghost dropdown-toggle">John Cruz</button>
+            <ul tabindex="0" class="dropdown-content menu shadow bg-base-100 rounded-box mt-3, bg-white">
+              <li><a href="#">Option 1</a></li>
+              <li><a href="#">Option 2</a></li>
+              <li><a href="#">Option 3</a></li>
+            </ul>
+          </div>
+          <div class="dropdown dropdown-end">
+            <div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
+              <div class="w-10 rounded-full">
+                <img alt="Tailwind CSS Navbar component" src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    <div style="text-align: center; padding-top: 1rem; font-size: 25px;">
+      <strong style="font-weight: bold;">Module Admin: Account Permission Management</strong>
     </div>
-    <div>
-      <button class="btn btn-sm" style="position: absolute; top: 0; right: 1rem; font-size: 12px; padding: 5px 10px;">Back</button>
+
+    <div style="display: flex; justify-content: flex-end; margin-top: 1rem;">
+      <button class="btn btn-sm mr-4" style="font-size: 12px; padding: 5px 10px;">Back</button>
     </div>
-</div>
+
+    </div>
