8822ade84ab776a465873b7c43cc0788379c047a
vincicoed <vinjove323@gmail.com>
2024-04-07 17:21:31 -0800

    feat:RolesList

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Livewire\Counter;
 use App\Livewire\Login;
 use App\Livewire\Saam;
+use App\Livewire\Roleslist;
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -14,3 +16,5 @@ Route::get('/counter', Counter::class);
 Route::get('/login', Login::class);
 
 Route::get('/saam', Saam::class);
+
+Route::get('/rl', roleslist::class);
