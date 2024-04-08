ab0fef50e0ae78077c74e0782c62c1bd1b41d4b3
aleurry <aleurrycasas@gmail.com>
2024-04-09 00:44:30 +0800

    fixed spacing or ano ganon

--- resources/views/livewire/saam.blade.php
+++ resources/views/livewire/saam.blade.php
@@ -7,9 +7,8 @@
         <svg class="w-8 h-8 text-neutral-500"  width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <path d="M9 13l-4 -4l4 -4m-4 4h11a4 4 0 0 1 0 8h-1" /></svg>
     </button>
 </div>
-<br>
 
-<b style="font-size: 30px">
+<b class="p-4" style="font-size: 30px">
     Super Admin: Account Management
 </b>
 
